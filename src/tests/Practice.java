4118ca541216a24c152789db6dc35f70a308e9bf
haticetin <56513772+haticetin@users.noreply.github.com>
2020-05-12 17:08:15 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -5,7 +5,9 @@ public class Practice {
     public static void main(String[] args) {
 
         // code here
-        // hello everyone
+        // hello EU2
+        
+        // This line is written from Github
 
     }
 }
